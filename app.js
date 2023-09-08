75b72763d9392c41c699204528f5815d67a0a66f
Musab Ahmed Khan <139049024+Musab-ahmed@users.noreply.github.com>
2023-09-08 04:14:00 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+//Chapter one
+// Q 1
+
+// alert ("Hello world");
+
+// Q 2
+
+// alert ("Error! Please Enter a valid password");
+
+//Q 3
+
+// alert ("welcome to JS Land....\nHappy coding!")
+
+// Q 4
+
+// alert ("Welcome to JS Land...");
+
+// alert ("Happy coding! \n ▢Prevent this page from creting additional dialogues.") 
+
+//Q 5
+
+// alert ("Hello...i can run js through my web browser's console");
+
+//Chapter 2
+
+//Q 1
+
+// var username; 
+
+//Q 2
+
+// var myNmae="Musab Ahmed Khan"
+
+//Q No 3
+// var message;
+// message ="Hello World";
+// alert(message);
+
+//Q No 4
+// var studentName="Johne Doe";
+// alert(studentName);
+// var age ="15 Years old";
+// alert(age);
+// var skill ="Certified Mobile Application Development";
+// alert(skill);
+
+
+//Q No 5
+
+//var message ="PIZZA\nPIZZ\nPIZ\nPI\nP";
+//alert(message);
+
+//Q No 6
+
+//var email ="musabahmedkhan089@gmail.com";
+//alert("My Email Address Is "+email);
+
+//Q No 7
+
+//var book ="A Smarter\nWay to learn javascript";
+//alert("I am trying to learn from the book "+book);
+
+//Q No 8
+//document.write("YAH! I CAN WRITE HTML Content Through Javascript");
+
+//Q No 9
+
+//var message = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+//alert(message);
+//document.write(message);
